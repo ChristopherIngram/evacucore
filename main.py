0fa4510c94d9d4eb3710730bd1a84144a964710e
Somotun <Victor.Somotun@Dillard.edu>
2016-12-03 16:58:56 -0600

    Trey Helped

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import webapp2
 import os
+import mimetypes
 from google.appengine.ext.webapp import template
 
 
@@ -9,6 +10,18 @@ class MainPage(webapp2.RequestHandler):
         self.response.out.write(template.render(path, {}))
 
 
+class Evacuspots (webapp2.RequestHandler):
+    def get (self):
+        self.response.headers["Content-Type"]="application/json"
+        path = os.path.join(os.path.dirname(__file__),'evacuspots.geojson')
+        self.response.out.write(template.render(path,{}))
+        
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    (r'/', MainPage),
+    (r'/api/evacuspots', Evacuspots),
 ], debug=True)
+
+
+
